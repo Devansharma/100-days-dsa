5b9ba9a626584bb6b9816e13f6165cf60aef07a8
Devansh Sharma <dsharma@sparkcognition.com>
2025-06-08 13:44:45 +0530

    remove duplicates

---
+++ arrays0/remove-duplicates.py
@@ -0,0 +1,11 @@
+a = [0,0,1,1,1,2,2,3,3,4]
+def removeDuplicates(arr):
+  x = 0
+  for i in range(len(arr)):
+    if arr[i] > arr[x]:
+      x = x + 1
+      arr[x] = arr[i]
+  return x+1
+  
+print(removeDuplicates(a))
+  
